427db7407ee85aff82b2b393eea6706997fcbe57
Sarvesh Sachin Kulkarni <62740647+SarveshK16@users.noreply.github.com>
2021-07-16 23:53:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const copyBtn = document.getElementById("copy-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+copyBtn.addEventListener("click", function (event) {
+    let copy_text = document.getElementsByTagName("ul")[0];
+    let range = document.createRange();
+    range.selectNode(copy_text);
+    window.getSelection().addRange(range);
+    try {
+        let successful = document.execCommand('copy');
+        let msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+})
